891c701b32374d8b33f2e8bd86b76d21f64c939e
Rodrigo <ro14208z@gmail.com>
2025-06-05 21:12:40 -0300

    pronto

--- index.php
+++ index.php
@@ -48,26 +48,26 @@
 <body>
 
     <div class="titulo">
-        <div class="link"><a href="exer1/index.php">exercício 1</a></div>
-        <div class="link"><a href="exer2/index.php">exercício 2</a></div>
-        <div class="link"><a href="exer3/index.php">exercício 3</a></div>
-        <div class="link"><a href="exe4/index.php">exercício 4</a></div>
-        <div class="link"><a href="exer5/index.php">exercício 5</a></div>
-        <div class="link"><a href="exer6/index.php">exercício 6</a></div>
-        <div class="link"><a href="exer7/index.php">exercício 7</a></div>
-        <div class="link"><a href="exer8/index.php">exercício 8</a></div>
-        <div class="link"><a href="exer9/index.php">exercício 9</a></div>
-        <div class="link"><a href="exer10/index.php">exercício 10</a></div>
-        <div class="link"><a href="exer11/index.php">exercício 11</a></div>
-        <div class="link"><a href="exer12/index.php">exercício 12</a></div>
-        <div class="link"><a href="exer13/index.php">exercício 13</a></div>
-        <div class="link"><a href="exer14/index.php">exercício 14</a></div>
-        <div class="link"><a href="exer15/index.php">exercício 15</a></div>
-        <div class="link"><a href="exer16/index.php">exercício 16</a></div>
-        <div class="link"><a href="exer17/index.php">exercício 17</a></div>
-        <div class="link"><a href="exer18/index.php">exercício 18</a></div>
-        <div class="link"><a href="exer19/index.php">exercício 19</a></div>
-        <div class="link"><a href="exer20/index.php">exercício20</a></div>
+        <div class="link"><a href="exer1/exer1.php">exercício 1</a></div>
+        <div class="link"><a href="exer2/exer2.php">exercício 2</a></div>
+        <div class="link"><a href="exer3/exer3.php">exercício 3</a></div>
+        <div class="link"><a href="exe4/exer4.php">exercício 4</a></div>
+        <div class="link"><a href="exer5/exer5.php">exercício 5</a></div>
+        <div class="link"><a href="exer6/exer6.php">exercício 6</a></div>
+        <div class="link"><a href="exer7/exer7.php">exercício 7</a></div>
+        <div class="link"><a href="exer8/exer8.php">exercício 8</a></div>
+        <div class="link"><a href="exer9/exer9.php">exercício 9</a></div>
+        <div class="link"><a href="exer10/exer10.php">exercício 10</a></div>
+        <div class="link"><a href="exer11/exer11.php">exercício 11</a></div>
+        <div class="link"><a href="exer12/exer12.php">exercício 12</a></div>
+        <div class="link"><a href="exer13/exer13.php">exercício 13</a></div>
+        <div class="link"><a href="exer14/exer14.php">exercício 14</a></div>
+        <div class="link"><a href="exer15/exer15.php">exercício 15</a></div>
+        <div class="link"><a href="exer16/exer16.php">exercício 16</a></div>
+        <div class="link"><a href="exer17/exer17.php">exercício 17</a></div>
+        <div class="link"><a href="exer18/exer18.php">exercício 18</a></div>
+        <div class="link"><a href="exer19/exer19.php">exercício 19</a></div>
+        <div class="link"><a href="exer20/exer20.php">exercício20</a></div>
 
     </div>
 
